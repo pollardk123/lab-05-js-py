06fa3961c831f58fb38fce383cf8046e49c852d0
kaitlyn pollard <pollardk836@gmail.com>
2021-10-06 17:33:03 -0400

    created a python version for madlibs

---
+++ madlibs.py
@@ -0,0 +1,9 @@
+noun = input("pizza");
+adjective = input("ridiculous");
+adverb = input("slowly");
+verb = input("running");
+
+print("On a tropical island" + str(noun) + ",");
+print("Underneath a" + adjective +" lava moon.");
+print("hangin'" + adverb +"with the hula dancers, ");
+print(str(verb) + "questions cause' they all got answers.");
