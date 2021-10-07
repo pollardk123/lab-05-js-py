c3b00c5b771469bd367c0500c5af87509bf9d418
kaitlyn pollard <pollardk836@gmail.com>
2021-10-06 21:59:28 -0400

    adjusted madlibs python file on atom by fixing a few lines

--- madlibs.py
+++ madlibs.py
@@ -1,9 +1,9 @@
-noun = input("pizza");
-adjective = input("ridiculous");
-adverb = input("slowly");
-verb = input("running");
+noun = input("pizza")
+adjective = input("ridiculous")
+adverb = input("slowly")
+verb = input("running")
 
-print("On a tropical island" + str(noun) + ",");
-print("Underneath a" + adjective +" lava moon.");
-print("hangin'" + adverb +"with the hula dancers, ");
-print(str(verb) + "questions cause' they all got answers.");
+print("On a tropical island" + str(noun) + ",")
+print("Underneath a" + str(adjective) +" lava moon.")
+print("hangin'" + str(adverb) +"with the hula dancers, ")
+print(str(verb) + "questions cause' they all got answers.")
